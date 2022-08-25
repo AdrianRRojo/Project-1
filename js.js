d5b5027fc2309cb9ddd7d1393f13d8c5c929b698
ADRIANRROJO <rojoadrian16@gmail.com>
2022-08-25 17:22:07 -0500

    Fixed major bugs / added start and reset button / added instructions / win and lose system

--- js.js
+++ js.js
@@ -3,18 +3,7 @@ const ctx = canvas.getContext('2d')
 canvas.setAttribute('height', getComputedStyle(canvas)['height'])
 canvas.setAttribute('width', getComputedStyle(canvas)['width'])
 
-const cassieImg = new Image()
-    cassieImg.src = "./HeroCards/traveler.png"
-const wrigglyImg = new Image()
-    wrigglyImg.src = "./HeroCards/wriggly.png"
-const daisyImg = new Image()
-    daisyImg.src = "./HeroCards/daisy.png"
-const zoeyImg = new Image()
-    zoeyImg.src = "./HeroCards/zoey.png"
-const dukeImg = new Image()
-    dukeImg.src = "./HeroCards/KingDuke.png"
-
-
+let x = document.querySelector('#reset')
 
 const heraticImg = new Image()
     heraticImg.src = "./Monsters/heratic.png"
@@ -35,47 +24,32 @@ function getRandomInt2(max) {
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
+  let aiScore = document.getElementById('aiScore')
+  let pScore = document.getElementById("pScore")
 
-const winner = function(cardName, btnName){
-    let monstersLvls = getRandomInt2(25)
-    let thislvl = getRandomInt(25)
-    let aiScore = document.getElementById('aiScore')
-    let pScore = document.getElementById("pScore")
-    if (monstersLvls > thislvl){
-        AICounter++
-        console.log("You lose! " + thislvl + "<- hero monster -> " + monstersLvls)
-        console.log(playerCounter + "<-PlayerCounter AICounter ->" + AICounter)
-        cardName.render(0,0,0,0)
-        btnName.disabled = true
-        // pScore.innerText = "PLAYER: " + playerCounter 
-        // aiScore.innerText =" Computer: " + AICounter
-    }else if(thislvl> monstersLvls){
-        playerCounter++ 
-        console.log("You Win!" + thislvl + "<- hero monster -> " + monstersLvls)
-        console.log(playerCounter + "<-PlayerCounter AICounter->" + AICounter)
-        cardName.render(340,300,125,150)
-        btnName.disabled = true
-        // pScore.innerText = "PLAYER: " + playerCounter
-        // aiScore.innerText =" Computer: " + AICounter
-    }else if(thislvl === monstersLvls){
-        ("TIE " + thislvl + "<- hero monster -> " + monstersLvls)
-        console.log(playerCounter + "<-PlayerCounter AICounter->" + AICounter)
-        btnName.disabled = false
-    }
-    if(playerCounter === 4){
-        console.log("THE PLAYER WON " + playerCounter)
-    }else if(AICounter === 4){
-        console.log("THE COMPUTER WON " + AICounter)
-    }else if(playerCounter === 3 && AICounter === 3){
-        console.log("IT WAS A TIE " + playerCounter + " " + AICounter)
-    }
-
-
-}
 
 // card constructor that will be used to generate cards for both the user and the AI
 
 class Cards{ 
+    constructor(name,lvl,imagePath,x,y,w,h){
+        this.name = name
+        this.lvl = lvl
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.image = new Image()
+        this.image.src = imagePath
+        this.image.onload = () => {
+            this.render()
+        }
+    }
+    render(){
+        ctx.drawImage(this.image,this.x,this.y,this.w,this.h)
+    }
+}
+
+class monsterCards{ 
     constructor(name, lvl,image){
     this.name = name
     this.lvl = lvl
@@ -86,34 +60,27 @@ class Cards{
     }
 }
 
-let duke = new Cards('KingDuke', 1, dukeImg)
-let zoey = new Cards('Zo-Zo', 1, zoeyImg)
-let daisy = new Cards('Dazed', 1, daisyImg)
-let wriggly = new Cards('Wiggle', 1, wrigglyImg)
-let cassie = new Cards('Cassie', 1, cassieImg)
-
-
-let heratic = new Cards('Heratic',1,heraticImg)
-let hunter = new Cards('Hunter',1,hunterImg)
-let knight = new Cards('Knight',1,knightImg)
-let mushroom = new Cards('mushrooom',1,mushroomImg)
-let skeleton = new Cards('skeleton',1,skeletonImg)
+ let heratic = new monsterCards('Heratic',1,heraticImg,340,300,125,150)
+ let hunter = new monsterCards('Hunter',1,hunterImg,340,300,125,150)
+ let knight = new monsterCards('Knight',1,knightImg,340,300,125,150)
+ let mushroom = new monsterCards('mushrooom',1,mushroomImg,340,300,125,150)
+ let skeleton = new monsterCards('skeleton',1,skeletonImg,340,300,125,150)
 
 // lists hero cards in an array so heroselector can pick through them
-let hero = [duke,
-    zoey,
-    daisy,
-    wriggly,
-    cassie,
+let hero = ['duke',
+    'zoey',
+    'daisy',
+    'wriggly',
+    'cassie',
 ]
 
-let monsters = [heratic,
-    hunter,
-    knight,
-    mushroom,
-    skeleton,
+ let monsters = [heratic,
+     hunter,
+     knight,
+     mushroom,
+     skeleton,
 ]
-let monsterCard = function(){
+let monsterCardPick = function(){
     let aiSelector = monsters[Math.floor(Math.random() * monsters.length)]
     if (aiSelector === heratic){
             heraticImg.onload = function(){
@@ -142,147 +109,210 @@ let monsterCard = function(){
             }
         }
     }
+
+const winner = function(btnName,pickedCard){
+        let monstersLvls = getRandomInt2(5)
+            monsterCardPick()
+            if (pickedCard.lvl < monstersLvls){
+                AICounter++
+                pickedCard.lvl - 1
+                console.log("You lose! " + pickedCard.lvl + "<- hero monster -> " + monstersLvls)
+                console.log(playerCounter + "<-PlayerCounter AICounter ->" + AICounter)
+                pScore.innerText = "PLAYER: " + playerCounter 
+                aiScore.innerText =" Computer: " + AICounter
+                if(pickedCard.lvl === 0){
+                    btnName.disabled = true
+                }
+            }else if(pickedCard.lvl > monstersLvls){
+                playerCounter++ 
+                pickedCard.Lvl + .5
+                monstersLvls + 1
+                monsterCardPick()
+                console.log("You Win!" + pickedCard.lvl + "<- hero monster -> " + monstersLvls)
+                console.log(playerCounter + "<-PlayerCounter AICounter->" + AICounter)
+                btnName.disabled = true
+                pScore.innerText = "PLAYER: " + playerCounter
+                aiScore.innerText =" Computer: " + AICounter
+            }else if(pickedCard.lvl === monstersLvls){
+                ("TIE " + pickedCard.lvl + "<- hero monster -> " + monstersLvls)
+                console.log(playerCounter + "<-PlayerCounter AICounter->" + AICounter)
+                btnName.disabled = false
+            }
+            if(playerCounter === 4){
+                console.log("THE PLAYER WON " + playerCounter)
+                x.style.display = 'block';
+            }else if(AICounter === 4){
+                console.log("THE COMPUTER WON " + AICounter)
+                x.style.display = 'block';
+            }else if(playerCounter === 3 && AICounter === 3){
+                console.log("IT WAS A TIE " + playerCounter + "<-player computer->" + AICounter)
+                x.style.display = 'block';
+            }
+        
+        
+        }
+// let round = 1;
+// let aiDiff = 0;
+// const roundChange = () => {
+//     if(playerCounter === 0 && AICounter === 0){
+//          round = 1;
+//          aiDiff = 0;
+//     }
+//     console.log(round)   
+//     for(aiDiff < round; aiDiff++;){
+//         aiDiff ++
+//         monsterCard()
+//          if(playerCounter >=  1){
+//         round = round + playerCounter 
+//         console.log("PlayerCounter: " + playerCounter + " AiCounter: " + "Round: " + round + " aiDiff: " + aiDiff)
+//     }else if(AICounter >= 1){
+//         round = round + AICounter
+//         console.log("PlayerCounter: " + playerCounter + "AiCounter: " + "Round: " + "aiDiff: " + aiDiff)
+//     }
+// }
+//     }
+    
+
+
 let playing = (btnName,pickedCard) =>{
     btnName.addEventListener('click', function(){
-        pickedCard.render(190,300,125,150) 
+      if(pickedCard === 'duke'){
+        const duke = new Cards('duke',1,"./HeroCards/KingDuke.png",190,300,125,150) 
+        winner(btnName, duke)
+        console.log(duke.lvl)
+      }else if(pickedCard === 'zoey'){
+        const zoey = new Cards('zoey', 1,"./HeroCards/zoey.png", 190,300,125,150)
+        winner(btnName, zoey)
+      }else if(pickedCard === 'daisy'){
+        const daisy = new Cards('daisy',1, "./HeroCards/daisy.png",190,300,125,150)
+        winner(btnName,daisy)
+      }else if(pickedCard === 'wriggly'){
+        const wriggly = new Cards('wriggly', 1, "./HeroCards/wriggly.png",190,300,125,150)
+        winner(btnName,wriggly)
+      }else if(pickedCard === 'cassie'){
+        const cassie = new Cards('cassie',1, "./HeroCards/traveler.png",190,300,125,150)
+        winner(btnName,cassie)
+      }
     })
     
 }
 
-
+//update array to be strings
+// make the deck 
+// constructor(name, lvl,imagePath,x,y,w,h){
 playedCard = () => {
-    
+    start.disabled = true
+    start.style.display = 'none';
     for(let i = 0; i <= 5; i++){
         let heroSelector = hero[Math.floor(Math.random() * hero.length)]
-        if (heroSelector === duke){
-    console.log(duke)
-    dukeImg.onload = function(){
-        if(i === 0){
-         duke.render(50,750,125,150)
-         playing(btnOne, duke)
-         winner(duke, btnOne)
-        }else if(i === 1){
-            duke.render(175,750,125,150)
-            playing(btnTwo, duke)
-            winner(duke, btnTwo)
-        }else if(i === 2){
-            duke.render(300,750,125,150)
-            playing(btnThree, duke)
-            winner(duke, btnThree)
-        }else if(i === 3){
-            duke.render(425,750,125,150)
-            playing(btnFour, duke)
-            winner(duke, btnFour)
-        }else if(i === 4){
-            duke.render(550,750,125,150)
-            playing(btnFive, duke)
-            winner(duke, btnFive)
-        }else if(i === 5){
-            duke.render(675,750,125,150)
-            playing(btnSix, duke)
-            winner(duke, btnSix)
+        if (heroSelector === 'duke'){
+            if(i === 0){
+               const duke = new Cards('duke','1',"./HeroCards/KingDuke.png",50,750,125,150)
+                playing(btnOne, 'duke')
+            }else if(i === 1){
+              const duke = new Cards('duke','1',"./HeroCards/KingDuke.png", 175,750,125,150)
+                playing(btnTwo, 'duke')
+            }else if(i === 2){
+                const duke = new Cards('duke','1',"./HeroCards/KingDuke.png", 300,750,125,150)
+                playing(btnThree, 'duke')
+            }else if(i === 3){
+                const duke = new Cards('duke','1',"./HeroCards/KingDuke.png", 425,750,125,150)
+                playing(btnFour, 'duke')
+            }else if(i === 4){
+                const duke = new Cards('duke','1',"./HeroCards/KingDuke.png", 550,750,125,150)
+                playing(btnFive, 'duke')
+            }else if(i === 5){
+                const duke = new Cards('duke','1',"./HeroCards/KingDuke.png", 675,750,125,150)
+                playing(btnSix, 'duke')
+            }
+    }else if(heroSelector === 'zoey'){
+            if(i === 0){
+                const zoey = new Cards('zoey', '1',"./HeroCards/zoey.png", 50,750,125,150)
+                playing(btnOne, 'zoey')
+            }else if(i === 1){
+                const zoey = new Cards('zoey', '1',"./HeroCards/zoey.png", 175,750,125,150)
+                playing(btnTwo, 'zoey')
+            }else if(i === 2){
+                const zoey = new Cards('zoey', '1',"./HeroCards/zoey.png", 300,750,125,150)
+                playing(btnThree, 'zoey')
+            }else if(i === 3){
+                const zoey = new Cards('zoey', '1',"./HeroCards/zoey.png", 425,750,125,150)
+                playing(btnFour, 'zoey')
+            }else if(i === 4){
+                const zoey = new Cards('zoey', '1',"./HeroCards/zoey.png", 550,750,125,150)
+                playing(btnFive, 'zoey')
+            }else if(i === 5){
+                const zoey = new Cards('zoey', '1',"./HeroCards/zoey.png", 675,750,125,150)
+                playing(btnSix, 'zoey')
         }
-}
-        }else if(heroSelector === zoey){
-    console.log(zoey)
-    zoeyImg.onload = function(){
+    }else if(heroSelector === 'daisy'){
         if(i === 0){
-        zoey.render(50,750,125,150)
-        playing(btnOne, zoey)
-    }else if(i === 1){
-        zoey.render(175,750,125,150)
-        playing(btnTwo, zoey)
-}   else if(i === 2){
-        zoey.render(300,750,125,150)
-        playing(btnThree, zoey)
-}   else if(i === 3){
-    zoey.render(425,750,125,150)
-    playing(btnFour, zoey)
-}   else if(i === 4){
-    zoey.render(550,750,125,150)
-    playing(btnFive, zoey)
-}else if(i === 5){
-    zoey.render(675,750,125,150)
-    playing(btnSix, zoey)
-}
-}
-        }else if(heroSelector === daisy){
-    console.log(daisy)
-    daisyImg.onload = function(){
+                const daisy = new Cards('daisy','1', "./HeroCards/daisy.png",50,750,125,150)
+                playing(btnOne, 'daisy')
+            }else if(i === 1){
+                const daisy = new Cards('daisy','1', "./HeroCards/daisy.png",175,750,125,150)
+                playing(btnTwo, 'daisy')
+            }else if(i === 2){
+                const daisy = new Cards('daisy','1', "./HeroCards/daisy.png",300,750,125,150)
+                playing(btnThree, 'daisy')
+            }else if(i === 3){
+                const daisy = new Cards('daisy','1', "./HeroCards/daisy.png",425,750,125,150)
+                playing(btnFour, 'daisy')
+            }else if(i === 4){
+                const daisy = new Cards('daisy','1', "./HeroCards/daisy.png",550,750,125,150)
+                playing(btnFive, 'daisy')
+            }else if(i === 5){
+                const daisy = new Cards('daisy','1', "./HeroCards/daisy.png",675,750,125,150)
+                playing(btnSix, 'daisy')
+            }
+    }else if(heroSelector === 'wriggly'){
         if(i === 0){
-        daisy.render(50,750,125,150)
-        playing(btnOne, daisy)
-        }else if(i === 1){
-            daisy.render(175,750,125,150)
-            playing(btnTwo, daisy)
-        }else if(i === 2){
-            daisy.render(300,750,125,150)
-            playing(btnThree, daisy)
-        }else if(i === 3){
-            daisy.render(425,750,125,150)
-            playing(btnFour, daisy)
-        }else if(i === 4){
-            daisy.render(550,750,125,150)
-            playing(btnFive, daisy)
-        }else if(i === 5){
-            daisy.render(675,750,125,150)
-            playing(btnSix, daisy)
+                const wriggly = new Cards('wriggly', 1, "./HeroCards/wriggly.png",50,750,125,150)
+                playing(btnOne, 'wriggly')
+            }else if(i === 1){
+                const wriggly = new Cards('wriggly', 1, "./HeroCards/wriggly.png",175,750,125,150)
+                playing(btnTwo, 'wriggly')
+            }else if(i === 2){
+                const wriggly = new Cards('wriggly', 1, "./HeroCards/wriggly.png",300,750,125,150) 
+                playing(btnThree, 'wriggly')
+            }else if(i === 3){
+                const wriggly = new Cards('wriggly', 1, "./HeroCards/wriggly.png",425,750,125,150)
+                playing(btnFour, 'wriggly')
+            }else if(i === 4){
+                const wriggly = new Cards('wriggly', 1, "./HeroCards/wriggly.png",550,750,125,150)
+                playing(btnFive, 'wriggly')
+            }else if(i === 5){
+                const wriggly = new Cards('wriggly', 1, "./HeroCards/wriggly.png",675,750,125,150)
+                playing(btnSix, 'wriggly')
+            }
+    }else if(heroSelector === 'cassie'){
+        if(i === 0){
+                const cassie = new Cards('cassie',1, "./HeroCards/traveler.png",50,750,125,150)
+                playing(btnOne, 'cassie')
+            }else if(i === 1){
+                const cassie = new Cards('cassie',1, "./HeroCards/traveler.png",175,750,125,150) 
+                playing(btnTwo, 'cassie')
+            }else if(i === 2){
+                const cassie = new Cards('cassie',1, "./HeroCards/traveler.png",300,750,125,150)
+                playing(btnThree, 'cassie')
+            }else if(i === 3){
+                const cassie = new Cards('cassie',1, "./HeroCards/traveler.png",425,750,125,150)
+                playing(btnFour, 'cassie')
+            }else if(i === 4){
+                const cassie = new Cards('cassie',1, "./HeroCards/traveler.png",550,750,125,150)
+                playing(btnFive, 'cassie')
+            }else if(i === 5){
+                const cassie = new Cards('cassie',1, "./HeroCards/traveler.png",675,750,125,150)
+                playing(btnSix, 'cassie')
+            }
         }
-    }
-        }else if(heroSelector === wriggly){
-    wrigglyImg.onload = function(){
-        console.log(wriggly)
-        if(i === 0){
-            wriggly.render(50,750,125,150)
-            playing(btnOne, wriggly)
-        }else if(i === 1){
-            wriggly.render(175,750,125,150)
-            playing(btnTwo, wriggly)
-        }else if(i === 2){
-            wriggly.render(300,750,125,150) 
-            playing(btnThree, wriggly)
-        }else if(i === 3){
-            wriggly.render(425,750,125,150)
-            playing(btnFour, wriggly)
-        }else if(i === 4){
-            wriggly.render(550,750,125,150)
-            playing(btnFive, wriggly)
-        }else if(i === 5){
-            wriggly.render(675,750,125,150)
-            playing(btnSix, wriggly)
+            }   
         }
-    }
-        }else if(heroSelector === cassie){
-    console.log(cassie)
-    cassieImg.onload = function(){
-        if(i === 0){
-            cassie.render(50,750,125,150)
-            playing(btnOne, cassie)
-        }else if(i === 1){
-            cassie.render(175,750,125,150) 
-            playing(btnTwo, cassie)
-        }else if(i === 2){
-            cassie.render(300,750,125,150)
-            playing(btnThree, cassie)
-        }else if(i === 3){
-            cassie.render(425,750,125,150)
-            playing(btnFour, cassie)
-        }else if(i === 4){
-            cassie.render(550,750,125,150)
-            playing(btnFive, cassie)
-        }else if(i === 5){
-            cassie.render(675,750,125,150)
-            playing(btnSix, cassie)
-        }
-    }
-        }   
-    }
-}
-
-playedCard()
 
 
 
+
+reset = () => {location.reload();}
 
 
 
